a138fc3118e71af3ebe14c5990ef4edcf6ae7d5d
brahmajit <brahmajit2123@gmail.com>
2020-07-20 21:26:31 +0530

    changed the color scheme a bit, moved to tamzen font, and a wallpaper

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -3,7 +3,6 @@ alias la='ls -a'
 alias diff='diff --color=auto'
 alias grep='grep  --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}'
 alias ip='ip -color=auto'
-alias vi="nvim"
 alias vim="nvim"
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias weather='curl wttr.in'
@@ -13,3 +12,4 @@ alias cpp='clang++ -g'
 export EDITOR=nvim
 export VISUAL=nvim
 export BROWSER=firefox
+export DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
