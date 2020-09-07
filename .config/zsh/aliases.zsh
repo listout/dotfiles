ac85db2ae6021bcb3c2f0c0e266e0f57e6f96a53
listout <noctura23@protonmail.com>
2020-09-07 16:29:28 +0530

    moved to nvm

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -14,3 +14,5 @@ export VISUAL=nvim
 export BROWSER=firefox
 export QT_QPA_PLATFORMTHEME=qt5ct
 export DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
