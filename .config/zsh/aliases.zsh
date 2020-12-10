6e2ace105a05f68f8dd7fc617abd08a258baf27e
listout <listout@protonmail.com>
2020-12-10 23:44:02 +0530

    nvim as default man pager

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -17,6 +17,8 @@ alias mkdir='mkdir -vp'
 alias mutt='neomutt'
 
 export EDITOR=/usr/bin/nvim
+export MANPAGER="/bin/sh -c \"col -b | /usr/bin/nvim -u NORC -c 'set ft=man ts=8 nomod nolist nonu noma' -\""
+export PAGER=/usr/bin/nvim
 export VISUAL=/usr/bin/nvim
 export BROWSER=/usr/bin/firefox
 export QT_QPA_PLATFORMTHEME=qt5ct
