1479af7b8739d97fd4c06ebdd61cd4f8e7114587
listout <listout@protonmail.com>
2020-12-27 19:08:50 +0530

    handle 'command not found' using pkgfile

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -2,3 +2,10 @@
 fpath+=$HOME/.config/zsh/pure
 autoload -U promptinit;promptinit
 prompt pure
+
+# handle command not found using pkgfile
+if [ -e /usr/share/doc/pkgfile/command-not-found.zsh ]; then
+	source /usr/share/doc/pkgfile/command-not-found.zsh
+else
+	echo "pkgfile not installed"
+fi
