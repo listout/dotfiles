573cfff16888796fe59870bc79de13d49c16591d
listout <listout@protonmail.com>
2021-02-24 20:26:15 +0530

    added class path for mariadb jdbc

--- .config/zsh/paths.zsh
+++ .config/zsh/paths.zsh
@@ -3,6 +3,7 @@ typeset -U PATH path
 path=("$HOME/.local/bin" "$path[@]")
 export PATH
 
+#export CLASSPATH=.:/usr/share/java/mariadb-jdbc/mariadb-java-client-2.7.1.jar
 export PATH=/home/gogo/.fnm:$PATH
 eval "$(fnm env)"
 
